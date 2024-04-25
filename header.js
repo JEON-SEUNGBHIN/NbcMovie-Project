f9da1e56a97403141b50331d771fd8d7f11453dd
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-04-25 20:59:59 +0900

    api 불러오기 완료

--- header.js
+++ header.js
@@ -4,3 +4,22 @@ fetch('./header.html')
         document.getElementById('header').innerHTML = html;
     })
     .catch(error => console.error('Error fetching header:', error));
+
+    async function search(event) {
+        event.preventDefault(); // 기본 제출 동작 방지
+    
+        // 입력 상자에서 입력된 검색어를 가져옴
+        const searchInput = document.getElementById('search').value.toLowerCase();
+    
+        // 영화 목록을 가져옴
+        const movies = await fetchMovies();
+    
+        // 영화 목록에서 검색어를 포함하는 영화들을 필터링하여 새로운 배열 생성
+        const filteredMovies = movies.filter(movie => {
+            return movie.title.toLowerCase().includes(searchInput);
+        });
+    
+        // 검색 결과를 화면에 표시
+        displayMovies(filteredMovies);
+    }
+    
--- movie.js
+++ movie.js
@@ -1,3 +1,4 @@
+//인기있는 영화 api 20개 가져오기
 const options = {
     method: 'GET',
     headers: {
@@ -11,5 +12,37 @@ fetch(
     'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
     options)
     .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then(data => {
+        // 영화 데이터를 받아왔을 때 실행할 코드
+        const movies = data.results; // API로부터 받은 영화 데이터 배열
+        
+        // 영화 목록을 순회하면서 HTML 요소를 생성하고 삽입
+        movies.forEach((movie, index) => {
+            const mainBody = document.querySelector('.main-body');
+            
+            // 새로운 item 요소 생성
+            const item = document.createElement('div');
+            item.classList.add('item');
+
+            // item 요소 내용 구성
+            item.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" id="movies-img">
+                <div class="item-body">
+                    <h2>${index + 1}</h2>
+                    <h3 class="movies-title">${movie.title}</h3>
+                    <h5 class="movies-content">${movie.overview}</h5>
+                    <div class="item-detail">
+                        <h4>개봉일: </h4> 
+                        <h4 id="movie-date">${movie.release_date}</h4>
+                        <i class="fa fa-star" id="scores"></i>
+                        <h5 id="movies-score">${movie.vote_average}</h5>
+                    </div>
+                </div>
+            `;
+            // 생성된 요소를 DOM에 추가
+            mainBody.appendChild(item);
+        });
+    })
+    .catch(error => {
+        console.error('영화 데이터를 가져오는 중 오류가 발생했습니다:', error);
+    });
